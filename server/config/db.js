19fbc83253e32fbeeb95fff3b1cd0f98f378f5bf
simran9524 <simrankalra9524@gmail.com>
2019-04-01 21:48:06 -0400

    api changes

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,6 @@
 */
 module.exports = {
 //"URI" : "mongodb://localhost/test",
-"URI" : "mongodb://<simran>:<simran1>@ds060977.mlab.com:60977/comp308-w2019-300991457-assignment2",
+"URI" : "mongodb://simran:simran1@ds060977.mlab.com:60977/comp308-w2019-300991457-assignment2",
 "secret": "SomeSecret"
 }
